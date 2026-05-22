5648a5e1cb088f9d5f82703faf681bd84de76fc8
Sargana Dev <dev@sargana.local>
2026-05-22 20:48:33 +0500

    mobile app

--- mobile/android/app/src/main/java/com/odin/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/odin/mobile/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
@@ -47,7 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
       }
 
       @Override
-      protected Boolean isHermesEnabled() {
+      protected boolean isHermesEnabled() {
         return BuildConfig.IS_HERMES_ENABLED;
       }
   });
@@ -61,15 +60,9 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false;
-    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
-    }
-    if (BuildConfig.DEBUG) {
-      ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
